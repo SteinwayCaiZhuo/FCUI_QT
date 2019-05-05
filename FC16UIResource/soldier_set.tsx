1e7bb21d56d10c2a1b7dc81f1fc0cce377f97668
SteinwayCaiZhuo <39693118+SteinwayCaiZhuo@users.noreply.github.com>
2019-05-05 18:14:05 +0800

    1.2.5

--- FC16UIResource/soldier_set.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="soldier_set" tilewidth="32" tileheight="32" tilecount="35" columns="7">
- <image source="tileset/soldier_set.png" width="224" height="160"/>
-</tileset>
